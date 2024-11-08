63115316d7cad10e9622c2b98da9846f3f4bb5de
Bereket Getachew <bakiget78@gmail.com>
2024-11-08 19:32:50 +0300

    deploying

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Element } from "react-scroll"; // Import Link and Element for smooth scrolling
+import { Element } from "react-scroll";
 import About from "./component/About";
 import Accordion from "./component/Accordion";
 import Contact from "./component/Contact";
@@ -12,27 +12,25 @@ import Services from "./component/Services";
 import Solutions from "./component/Solutions";
 import Team from "./component/Team";
 import { DotLoader } from "react-spinners";
-// import FloatingIcon from "./component/FloatingIcon";
+import FloatingIcon from "./component/FloatingIcon";
 
 const Pages = () => {
   const [loading, setLoading] = useState(true);
   const [fadeOut, setFadeOut] = useState(false);
 
   useEffect(() => {
-    // Set a timer to start fade-out effect after 1 second
     const timer = setTimeout(() => {
       setFadeOut(true);
     }, 1000);
 
-    return () => clearTimeout(timer); // Clear the timer if the component is unmounted
+    return () => clearTimeout(timer); 
   }, []);
 
   useEffect(() => {
     if (fadeOut) {
-      // Set another timer to hide the spinner after the fade-out duration
       const timer = setTimeout(() => {
         setLoading(false);
-      }, 1000); // Match this duration with your CSS transition duration
+      }, 1000); 
 
       return () => clearTimeout(timer);
     }
@@ -76,7 +74,7 @@ const Pages = () => {
           <Element name="footer">
             <Footer5 />
           </Element>
-          {/* <FloatingIcon /> */}
+          <FloatingIcon />
         </div>
       )}
     </div>
